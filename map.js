4b4fc0502157fc3d3cf839dbe1257e1fcf3f928c
JayManjrekar0827 <jmanjrekar@ucsd.edu>
2026-05-16 18:42:21 -0700

    Add Mapbox GL JS and first map

---
+++ map.js
@@ -0,0 +1,17 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Replace with your Mapbox public token from https://account.mapbox.com/
+mapboxgl.accessToken = 'YOUR_ACCESS_TOKEN_HERE';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
